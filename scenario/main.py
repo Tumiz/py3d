ce72c6954baedaa3fd0b9f1981d141e79fbd6a93
huangjiajun <huangjiajun@holomatic.com>
2020-04-06 20:26:30 +0800

    rotation and server

--- scenario/main.py
+++ scenario/main.py
@@ -1,35 +1,128 @@
 # coding: utf-8
-from torch import tensor,sin,cos,Tensor,ones
 from .server import *
+from math import *
+import torch
 
 class Vector3: 
     def __new__(cls,x=0,y=0,z=0):
-        return Tensor([float(x),float(y),float(z)])
+        return torch.Tensor([float(x),float(y),float(z)])
+
+class Rotation:
+    def __init__(self):
+        self.matrix=torch.eye(4)
+    @staticmethod
+    def eular(x=0,y=0,z=0):
+        ret=Rotation()
+        ret.matrix=Rotation.rz_matrix(z).mm(Rotation.ry_matrix(y)).mm(Rotation.rx_matrix(x))
+        return ret
+    @staticmethod
+    def quaternion(x,y,z,w):
+        ret=Rotation()
+        ret.matrix=torch.tensor([
+            [2*(pow(x,2)+pow(w,2))-1,2*(x*y-w*z),2*(x*z+w*y),0],
+            [2*(x*y+w*z),2*(pow(w,2)+pow(y,2))-1,2*(y*z-w*x),0],
+            [2*(x*z-w*y),2*(y*z+w*x),2*(pow(w,2)+pow(z,2))-1,0],
+            [0,0,0,1]
+        ])
+        return ret
+    @staticmethod
+    def axis_angle(axis,angle):
+        axis=axis/axis.norm()
+        w=cos(angle/2)
+        x,y,z=sin(angle/2)*axis
+        return Rotation.quaternion(x,y,z,w)
+    @staticmethod
+    def rx_matrix(x):
+        return torch.tensor([
+            [1, 0, 0,0],
+            [0, cos(x), -sin(x),0],
+            [0, sin(x), cos(x),0],
+            [0,0,0,1]
+        ])     
+    @staticmethod
+    def ry_matrix(y):
+        return torch.tensor([
+            [cos(y),0,sin(y),0],
+            [0,1,0,0],
+            [-sin(y),0,cos(y),0],
+            [0,0,0,1]
+        ])
+    @staticmethod
+    def rz_matrix(z):
+        return torch.tensor([
+            [cos(z),-sin(z),0,0],
+            [sin(z),cos(z),0,0],
+            [0,0,1,0],
+            [0,0,0,1]
+        ])
+    @staticmethod
+    def eular2quaternion(rx,ry,rz):
+        sx,cx=sin(rx/2),cos(rx/2)
+        sy,cy=sin(ry/2),cos(ry/2)
+        sz,cz=sin(rz/2),cos(rz/2)
+        w=cx*cy*cz+sx*sy*sz
+        z=cx*cy*sz-sx*sy*cz
+        y=cx*sy*cz+sx*cy*sz
+        x=sx*cy*cz-cx*sy*sz
+        return x,y,z,w
+    def to_eular(self):
+        y=atan2(-self.matrix[2,0],sqrt(pow(self.matrix[0,0],2)+pow(self.matrix[1,0],2)))
+        z=atan2(self.matrix[1,0]/cos(y),self.matrix[0,0]/cos(y))
+        x=atan2(self.matrix[2,1]/cos(y),self.matrix[2,2]/cos(y))
+        return torch.tensor([x,y,z])
+    def to_quaternion(self):
+        w=0.5*sqrt(max(self.matrix[0,0]+self.matrix[1,1]+self.matrix[2,2]+1,0))
+        x=(self.matrix[2,1]-self.matrix[1,2])/4/w
+        y=(self.matrix[0,2]-self.matrix[2,0])/4/w
+        z=(self.matrix[1,0]-self.matrix[0,1])/4/w
+        return torch.tensor([x,y,z,w])
+    def to_axis_angle(self):
+        angle=acos((self.matrix[0,0]+self.matrix[1,1]+self.matrix[2,2]-1)/2)
+        axis=torch.tensor([
+            self.matrix[2,1]-self.matrix[1,2],
+            self.matrix[0,2]-self.matrix[2,0],
+            self.matrix[1,0]-self.matrix[0,1]
+        ])
+        return axis,angle
+    def rotate_x(self,angle):
+        self.matrix=Rotation.rx_matrix(angle).mm(self.matrix)
+        return self
+    def rotate_y(self,angle):
+        self.matrix=Rotation.ry_matrix(angle).mm(self.matrix)
+        return self
+    def rotate_z(self,angle):
+        self.matrix=Rotation.rz_matrix(angle).mm(self.matrix)
+        return self
+    def rotate_axis(self,axis,angle):
+        self.matrix=Rotation.axis_angle(axis,angle).matrix.mm(self.matrix)
+        return self
+    def info(self):
+        return self.to_quaternion().tolist()
 
 class Transform:
-    def __init__(self,position=Vector3(),rotation=Vector3(),scale=Vector3(1,1,1)):
+    def __init__(self,position=Vector3(),rotation=Rotation(),scale=Vector3(1,1,1)):
         self.position=position
         self.rotation=rotation
         self.scale=scale
         
     def transform_position(self,loc):#local position
-        tmp=tensor([
+        tmp=torch.tensor([
             [loc[0]],
             [loc[1]],
             [loc[2]],
             [1]
             ])
-        r=self.translation_matrix().mm(self.rotation_x_matrix()).mm(self.rotation_y_matrix()).mm(self.rotation_z_matrix()).mm(self.scaling_matrix()).mm(tmp)[0:3,0]
+        r=self.translation_matrix().mm(self.rotation.matrix).mm(self.scaling_matrix()).mm(tmp)[0:3,0]
         return r
     
     def transform_rotation(self,rot):#local rotation
-        tmp=tensor([
+        tmp=torch.tensor([
             [rot[0]]
         ])
         pass
 
     def translation_matrix(self):
-        return tensor([
+        return torch.tensor([
             [1,0,0,self.position[0]],
             [0,1,0,self.position[1]],
             [0,0,1,self.position[2]],
@@ -37,57 +130,26 @@ class Transform:
         ])
 
     def scaling_matrix(self):
-        return tensor([
+        return torch.tensor([
             [self.scale[0],0,0,0],
             [0,self.scale[1],0,0],
             [0,0,self.scale[2],0],
             [0,0,0,1]
         ])
     
-    def rotation_x_matrix(self):
-        x=self.rotation[0]
-        return tensor([
-            [1,0,0,0],
-            [0,cos(x),-sin(x),0],
-            [0,sin(x),cos(x),0],
-            [0,0,0,1]
-        ])
-
-    def rotation_y_matrix(self):
-        y=self.rotation[1]
-        return tensor([
-            [cos(y),0,sin(y),0],
-            [0,1,0,0],
-            [-sin(y),0,cos(y),0],
-            [0,0,0,1]
-        ])
-
-    def rotation_z_matrix(self):
-        z=self.rotation[2]
-        return tensor([
-            [cos(z),-sin(z),0,0],
-            [sin(z),cos(z),0,0],
-            [0,0,1,0],
-            [0,0,0,1]
-        ])
-    
     def info(self):
-        return {"position":self.position.tolist(),"rotation":self.rotation.tolist(),"scale":self.scale.tolist()}
+        return {"position":self.position.tolist(),"rotation":self.rotation.info(),"scale":self.scale.tolist()}
     
     def __repr__(self):
         return json.dumps(self.info())
     
-class Scenario(object):
-    _instance=None
-    def __init__(self):
-        pass
-    def __new__(cls):#single instance
-        if cls._instance is None:
-            cls._instance=object.__new__(cls)
-            cls.t=0
-            cls.objects=set()
-            cls.server=Server()
-        return cls._instance
+class Scenario:
+    server=None
+    def __init__(self):#single instance
+        self.t=0
+        self.objects=set()
+        if Scenario.server is None:
+            Scenario.server=Server()
 
     def add(self,*objs):
         for obj in objs:
@@ -101,13 +163,17 @@ class Scenario(object):
         for obj in self.objects:
             obj.step(dt)
         self.t+=dt
+
+    def info(self):
+        ret=dict()
+        for obj in self.objects:
+            ret[obj.id]=obj.info()
+        return ret
         
     def render(self):
-        if not self.server.is_alive():
-            self.server.start()
-            self.server.open_web()
-        for obj in self.objects:
-            self.server.send_msg(obj.info())
+        if not Scenario.server.is_alive():
+            Scenario.server.start()
+        Scenario.server.send_msg(self.info())
         
 class Object3D(Transform):
     def __init__(self):
@@ -146,7 +212,7 @@ class Object3D(Transform):
     
 class Color:
     def __new__(cls,r=0,g=0,b=0,a=1):
-        return tensor([float(r),float(g),float(b),float(a)])
+        return torch.tensor([float(r),float(g),float(b),float(a)])
         
 class Cube(Object3D):
     def __init__(self):
--- scenario/server.py
+++ scenario/server.py
@@ -31,35 +31,36 @@ class WebsocketHandler(tornado.websocket.WebSocketHandler):
 
 class Server(threading.Thread):
     def __init__(self):
-        super().__init__()
+        threading.Thread.__init__(self)
         self.setDaemon(True)
-        self.static_path=os.path.join(os.path.dirname(__file__), "static")
-        self.template_path=os.path.join(os.path.dirname(__file__), "template")
-        self.app = tornado.web.Application([
-                (r"/", IndexHandler),
-                (r"/ws", WebsocketHandler),
-                (r"/(.*)",tornado.web.StaticFileHandler)      
-            ],
-            static_path = self.static_path,
-            template_path = self.template_path,
-            debug = True
-            )
         
     def open_web(self):
         webbrowser.open("http://localhost:8080/",new=0,autoraise=False)
 
     def send_msg(self,msg):
         t=0
+        if len(WebsocketHandler.users)==0:
+            self.open_web()
         while len(WebsocketHandler.users) == 0:
             print("waiting for web to open, "+str(t)+" s\r",end='')
             time.sleep(0.1)
             t+=0.1
         WebsocketHandler.send_msg(msg)
-        
+
     def run(self):
-        # print("start")
         loop=asyncio.new_event_loop()
         asyncio.set_event_loop(loop) #允许server在子线程中运行
-        http_server = tornado.httpserver.HTTPServer(self.app)
+        static_path=os.path.join(os.path.dirname(__file__), "static")
+        template_path=os.path.join(os.path.dirname(__file__), "template")
+        app = tornado.web.Application([
+                (r"/", IndexHandler),
+                (r"/ws", WebsocketHandler),
+                (r"/(.*)",tornado.web.StaticFileHandler)      
+            ],
+            static_path = static_path,
+            template_path = template_path,
+            debug = True
+        )
+        http_server = tornado.httpserver.HTTPServer(app)
         http_server.listen(8080)
         tornado.ioloop.IOLoop.current().start() 
