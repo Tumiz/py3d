b713985a1a703da53d056793722c1c12dcfdb2eb
Tumiz <hh11698@163.com>
2022-09-05 11:02:09 -0500

    fix car model (#34)
    * fix car model
    
    * update docs

--- py3d/py3d/core.py
+++ py3d/py3d/core.py
@@ -48,12 +48,14 @@ def render(**args):
         open("debug.html", "w").write(html)
     return display(HTML(html))
 
-
 class Data(numpy.ndarray):
     BASE_SHAPE = ()
 
     def __new__(cls, data: list | numpy.ndarray = [], n=()):
         return numpy.tile(data, n + (1,)).view(cls)
+
+    def __imatmul__(self, value) -> Data:
+        return self @ value
 
     @property
     def n(self):
@@ -648,21 +650,21 @@ class LineSegment(Point):
         self.vertice[..., 1::2, :].squeeze()[:] = v
 
     @classmethod
-    def Car(cls, *n):
+    def Car(cls) -> LineSegment:
         ret = Vector3([
             [-1, 1, 0],
             [-1, 1, 0.8],
             [0, 1, 1.6],
             [2, 1, 1.6],
-            [5, 1, 0.8],
-            [5, 1, 0],
+            [4, 1, 0.8],
+            [4, 1, 0],
             [-1, -1, 0],
             [-1, -1, 0],
             [-1, -1, 0.8],
             [0, -1, 1.6],
             [2, -1, 1.6],
-            [5, -1, 0.8],
-            [5, -1, 0],
+            [4, -1, 0.8],
+            [4, -1, 0],
             [-1, -1, 0]
         ])
         index = [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 0, 6, 7, 7, 8, 8, 9, 9,
