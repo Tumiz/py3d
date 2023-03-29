b66a8b0808ba483c1a9fa230f5a7aa5f0491c8a9
tumiz <hh11698@163.com>
2023-03-30 00:08:03 +0800

    add some comments

--- py3d/py3d/__init__.py
+++ py3d/py3d/__init__.py
@@ -1,3 +1,4 @@
+'''
+Welcome to py3d world, py3d is pure python and 3d computational geometry library. Please visit https://tumiz.github.io/scenario/ for more information
+'''
 from .core import *
-
-print("Welcome to py3d world, please visit https://tumiz.github.io/scenario/ for more information")
